4dc642567eece1a003a1a30e7c3b977f3dde96a4
Chris Kim <cj3kim@gmail.com>
2015-02-23 22:42:25 -0800

    Started writing db stuff

---
+++ config/db/config.js
@@ -0,0 +1,16 @@
+var DbConfig = {
+  development: {
+    client: 'pg',
+    connection: {
+      host     : '127.0.0.1',
+      user     : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test',
+      charset  : 'utf8'
+    }
+  },
+  staging: {},
+  production: {}
+};
+
+module.exports = DbConfig;
