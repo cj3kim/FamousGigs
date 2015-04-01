7aede0cc8e1a5b5c4b26b185cc1c891ac91b346b
Chris Kim <chris@wundercode.net>
2015-03-31 18:36:17 -0700

    Removed famous-flex

--- famous-flex/src/ScrollView.js
+++
@@ -1,7 +0,0 @@
-// Deprecated, require FlexScrollView instead
-/*global define, console*/
-/*eslint no-use-before-define:0, no-console:0 */
-    if (console.warn) {
-        console.warn('file famous-flex/ScrollView has been deprecated, use famous-flex/FlexScrollView instead');
-    }
-    module.exports = require('./FlexScrollView');
