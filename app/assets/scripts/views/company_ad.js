01dd46b75fb6018db7ccfd69e40aaf851715e0a5
Chris Kim <cj3kim@gmail.com>
2015-04-10 16:16:25 -0700

    Utilized dangerous html to display views for now. Fix later

--- app/assets/scripts/views/company_ad.js
+++ app/assets/scripts/views/company_ad.js
@@ -2,11 +2,18 @@ var React = require('react');
 
 var CompanyAd = React.createClass({
   render: function () {
+    var _this = this;
+    function createMarkup() { 
+      return {
+        __html: _this.props.description
+      }; 
+    };
+    
     return (
       <div className="stop-gap-div"> 
         <h3 className='title'>{ this.props.title }</h3>
         <p>{ this.props.job_location }</p>
-        <p>{ this.props.description }</p>
+        <div dangerouslySetInnerHTML={createMarkup()} />
       </div>
     );
   }
