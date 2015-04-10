20d0dd1d8495e8907e95b847689634308f5c45f6
Chris Kim <cj3kim@gmail.com>
2015-04-10 11:02:28 -0700

    Migrated updates to company_ads. Flush it later and reset company_name in company_ads to NOT NULL.

---
+++ config/db/migrations/20150410104318_update_ad_details.js
@@ -0,0 +1,30 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  
+  return knex.schema.table('company_ads', function (t) {
+    t.dropColumn('user_id')
+    t.renameColumn('email', 'contact_email');
+
+    t.boolean('full_time').nullable();
+    t.boolean('part_time').nullable();
+    t.boolean('gig').nullable();
+    t.boolean('onsite').nullable();
+    t.boolean('remote').nullable();
+
+    t.string('logo_url').nullable()
+
+    // Since rows already exist in the table, the ALTER statment is attempting 
+    // to insert NULL values for columsn with existing rows.
+    // Not nullable but fix at a later point. 
+
+    t.string('company_name'); 
+    t.string('company_email');
+    t.string('company_link');
+    t.text('additional_notes');;
+  });
+};
+
+exports.down = function(knex, Promise) {
+  
+};
