f02fb23604d00c8231c9eb3b37f46e8dcaffb431
Chris Kim <cj3kim@gmail.com>
2015-04-30 11:11:39 -0700

    We have the Users Model

---
+++ app/models/users.js
@@ -0,0 +1,31 @@
+var env = process.env.NODE_ENV;
+var dbConfig = require('../../config/db/knexfile')[env];
+var knex = require('knex')(dbConfig);
+var bookshelf = require('bookshelf')(knex);
+
+var Users = bookshelf.Model.extend({
+    tableName: 'users',
+    initialize: function () {
+      this.on('saving', this.validateSave);
+    },
+
+    validateSave: function () {
+      return checkit(rules).run(this.attributes);
+    },
+  }, 
+
+  {
+    login: Promise.method(function (email, password) {
+      if (!email || !password) throw new Error("Email and password are both required.");
+      return new this({ email: email.toLowerCase().trim() })
+        .fetch({ require: true })
+        .tap(function (user) {
+          return bcrypt.compareAsync(customer.get('password'), password);
+        });
+    })
+  }
+);
+
+
+
+module.exports = Users;
