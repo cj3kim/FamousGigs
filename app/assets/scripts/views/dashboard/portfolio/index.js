7c0cdf1529fc2c72814ece09fab249d93076b003
Chris Kim <cj3kim@gmail.com>
2015-05-12 22:43:45 -0700

    Switched out to portfolio in dashboard

--- app/assets/scripts/views/dashboard/portfolio/index.js
+++ app/assets/scripts/views/dashboard/portfolio/index.js
@@ -1,15 +1,4 @@
-var FlexColumns    = require('../../flex-columns/index');
-var WorkUpload = require('../../work-upload/index');
 
-
-
-var portfolio = new FlexColumns({
-  marginTop: 20,
-  gutterCol: 20,
-  gutterRow: 20
-});
-
-var workUpload = new WorkUpload();
-portfolio.createCol(235).addColNode(0, workUpload, 235);
+var portfolio = require('../../../starters/works');
 
 module.exports = portfolio;
