dcbea4ed7950794c3cd1fc9e2b34bc41642a8987
Chris Kim <chris@wundercode.net>
2015-05-19 21:36:13 -0700

    Refactored profile_basics

--- app/assets/scripts/views/dashboard/profile_basics.js
+++ app/assets/scripts/views/dashboard/profile_basics.js
@@ -1,11 +1,59 @@
-
 var React = require('react');
-
 var ReactSurface = require('react-surface');
 var View = require('famous/core/View');
-var ProfileBasicsReact = require('../../react_views/dashboard/profile_basics');
+var FamousGigsDispatcher = require('../../dispatcher');
+var user = require('../../models/singleton/user');
 
-var User = require('../../models/singleton/user');
+var ProfileBasicsReact = React.createClass({
+  componentDidMount: function () {
+    var _this = this;
+
+    _this.setState({ email: user.get('email')});
+
+  },
+
+  getInitialState: function () {
+    return {
+      disabled: '',
+      email: user.get('email'),
+    };
+  },
+
+  handleEmailChange: function (event) {
+    this.setState({email: event.target.value});
+  },
+  render: function () {
+    var email = this.state.email;
+
+    return (
+      <div  className="profile stop-gap-div">
+        <div className="title row">
+          <span>Profile Basics</span>
+        </div>
+        <div id="profile-basics" className="form row">
+          <form action="">
+            <table border="0">
+              <tr>
+                <td><label for="name">user name</label></td>
+                <td><input type="text" name="name" disabled={this.state.disabled }/></td>
+              </tr>
+
+              <tr>
+                <td><label for="email">email</label></td>
+                <td><input type="text" name="email" value={email} disabled={this.state.disabled } /></td>
+                </tr>
+              <tr>
+                <td><label for='image-upload'>Image Upload</label></td>
+                <td><input type='file' name='image-upload'/> </td>
+                </tr>
+            </table>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
 
 function ProfileBasics() {
   View.apply(this, arguments);
