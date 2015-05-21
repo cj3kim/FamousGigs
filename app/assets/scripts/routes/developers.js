04d21fe46c6e2e357d0fdc55e6ba87f1f43ac373
Chris Kim <cj3kim@gmail.com>
2015-05-21 02:21:40 -0700

    Developer Details can be scrolled through, but Engine doubly pipes to both nested scroll views. Fix this immediately

--- app/assets/scripts/routes/developers.js
+++ app/assets/scripts/routes/developers.js
@@ -1,7 +1,9 @@
+var Engine = require('famous/core/Engine');
 var Easing     = require('famous/transitions/Easing');
 var RenderNode = require('famous/core/RenderNode');
 var Modifier   = require('famous/core/Modifier');
 var Transform  = require('famous/core/Transform');
+var ScrollView = require('famous/views/ScrollView');
 
 var developerScrollView = require('../starters/developer_ads');
 var DevProfile = require('../views/developer-details/index');
@@ -17,12 +19,17 @@ module.exports = function (page, obj) {
   });
 
   var devProfile = new DevProfile();
+  var scrollview = new ScrollView();
+  scrollview.sequenceFrom([devProfile]);
+
+
+  Engine.pipe(scrollview);
 
   page('/developers/:id', function (ctx) {
     var id = ctx.params.id;
     var developer = developers.get(id);
 
     devProfile.update(developer);
-    bodyRC.show(devProfile);
+    bodyRC.show(scrollview);
   });
 }
