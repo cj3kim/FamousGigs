8295c3a3874fbef4b5c83d95946ce18108994db7
Chris Kim <cj3kim@gmail.com>
2015-05-24 18:57:57 -0700

    Adjusted the knexfile for staging

--- config/db/knexfile.js
+++ config/db/knexfile.js
@@ -1,14 +1,30 @@
 // Update with your config settings.
 
-module.exports = {
 
+var FG_PG_PASSWORD;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    FG_PG_PASSWORD = 'famous_givs_dev'
+    break;
+  case 'staging':
+    FG_PG_PASSWORD = process.env.FG_STAGING_PG_PASSWORD
+    break;
+  case 'production':
+    FG_PG_PASSWORD = process.env.FG_PRODUCTION_PG_PASSWORD
+    break
+  default:
+    // code
+}
+
+module.exports = {
   development: {
     client: 'pg',
     connection: {
       host     : '127.0.0.1',
       port: 5432,
       user     : 'famous_gigs_admin',
-      password : 'famous_gigs_dev',
+      password : FG_PG_PASSWORD,
       database : 'famous_gigs_development',
       charset  : 'utf8'
     },
@@ -21,15 +37,18 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'famous_gigs_staging',
+      port: 5432,
+      user:     'famous_gigs_admin',
+      password: FG_PG_PASSWORD,
+      charset  : 'utf8'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory:'./migrations',
       tableName: 'knex_migrations'
     }
   },
@@ -37,15 +56,16 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'famous_gigs_production',
+      user:     'famous_gigs_admin',
+      password:  FG_PG_PASSWORD
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory:'./migrations',
       tableName: 'knex_migrations'
     }
   }
