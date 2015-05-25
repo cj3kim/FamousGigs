5887b48b6104b058ed75d6646fc114083cc60312
Chris Kim <cj3kim@gmail.com>
2015-05-24 19:11:33 -0700

    Adjusted migrations file to deal with email

--- config/db/migrations/20150303200206_users.js
+++ config/db/migrations/20150303200206_users.js
@@ -3,7 +3,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(t) {
     t.increments().primary();
-    t.string('email').notNullable();
+    //t.string('email').notNullable();
     t.string('user_name').nullable();
     t.dateTime('created_at').notNull();
     t.dateTime('updated_at').nullable();
