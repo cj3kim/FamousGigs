8e950a3fa8e642b61721da3c7a1e5339b094a139
Chris Kim <cj3kim@gmail.com>
2015-05-27 17:33:16 -0700

    Created a migration to add more user detail columns.

---
+++ config/db/migrations/20150527171757_AddDetailAttributesToUsers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function (t) {
+    t.string('city');
+    t.string('state');
+    t.string('country');
+    t.string('about_tagline');
+    t.text('bio');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', function (t) {
+    t.dropColumn('city');
+    t.dropColumn('state');
+    t.dropColumn('country');
+    t.dropColumn('about_tagline');
+    t.text('bio');
+  });
+};
