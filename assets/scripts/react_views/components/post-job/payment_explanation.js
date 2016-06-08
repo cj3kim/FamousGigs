76cb92ce56c00cfbcc6063ca1b9d5dcdea3ed1ca
Chris Kim <cj3kim@gmail.com>
2016-06-07 20:22:30 -0700

    Changed pricing copy.

--- assets/scripts/react_views/components/post-job/payment_explanation.js
+++ assets/scripts/react_views/components/post-job/payment_explanation.js
@@ -1,9 +1,9 @@
-var React = require('react');
+var React = require("react");
 var GeneralContent = require("../../new_general_content");
 var PaymentExplanation = React.createClass({
   render: function () {
     return (
-      <GeneralContent className='payment-explanation' headerName="Simple Pricing">
+      <GeneralContent className="payment-explanation" headerName="Pricing">
         <ul>
           <li>$50 for Full-Time/Freelance Posts</li>
           <li>Active for Two Months</li>
